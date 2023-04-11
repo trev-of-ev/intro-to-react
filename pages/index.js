a61babc63c593bf8aa37b3fb01d2032b214614a6
Trevor Guinn <tdguinn87@gmail.com>
2023-04-10 20:00:16 -0500

    Counter Setup

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,20 @@
-// import { Button } from 'react-bootstrap'; // TODO: COMMENT IN FOR AUTH
-// import { signOut } from '../utils/auth'; // TODO: COMMENT IN FOR AUTH
-// import { useAuth } from '../utils/context/authContext'; // TODO: COMMENT IN FOR AUTH
+import { useState } from 'react';
 
 function Home() {
-  // const { user } = useAuth(); // TODO: COMMENT IN FOR AUTH
+  const [value, setValue] = useState(false);
 
-  const user = { displayName: 'Dr. T' }; // TODO: COMMENT OUT FOR AUTH
+  const handleClick = () => {
+    setValue((prevState) => prevState + 1);
+  };
+
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
-    </div>
+    <>
+      <h1>Counter Title</h1>
+      <h2>{value}</h2>
+      <button type="button" onClick={handleClick}>Increment</button>
+      <button type="button" onClick={() => setValue((prevState) => prevState - 1)}>Decrement</button>
+      <button type="button" onClick={() => setValue(5)}>Reset</button>
+    </>
   );
 }
 
